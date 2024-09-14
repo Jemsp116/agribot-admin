8c4959d5f38a32714767c7a50e0fb322d31060dc
hackani <kanishkchaudhary2005@gmail.com>
2024-09-14 23:06:13 +0530

    error fixed

--- app/api/robots/route.js
+++ app/api/robots/route.js
@@ -2,6 +2,39 @@ import { mongooseConnect } from "@/lib/mongoose";
 import Robot from "@/models/RobotModel";
 import { NextResponse } from "next/server";
 import Message from "@/models/MessageModel";
+import Cors from 'cors';
+
+// List of allowed origins
+const allowedOrigins = ['http://localhost:3000', 'https://agribot.vercel.app'];
+
+// Initializing the cors middleware
+const cors = Cors({
+  origin: (origin, callback) => {
+    // Allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    } else {
+      return callback(new Error('Not allowed by CORS'));
+    }
+  },
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,  // Optional if you need to allow credentials like cookies
+  allowedHeaders: ['Content-Type', 'Authorization'],
+});
+
+// Helper function to run middleware
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+      return resolve(result);
+    });
+  });
+}
+
 
 const getRobots = async (req) => {
     const { searchParams } = new URL(req.url);
@@ -22,6 +55,9 @@ const getRobots = async (req) => {
         return NextResponse.json({ message: "Internal server error" }, { status: 500 });
     }
 };
+
+
+
 
 const deleteRobot = async (req) => {
     const { searchParams } = new URL(req.url);
@@ -96,6 +132,8 @@ const updateRobot = async (req) => {
 
 // Main handler function
 const robotHandler = async (req) => {
+    // Run the CORS middleware
+    await runMiddleware(req, res, cors);
     await mongooseConnect();
 
     const method = req.method;
