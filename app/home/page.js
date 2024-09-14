4f73fa3fedce9bb7d4eef0cd6c50eacdbe248f64
hackani <kanishkchaudhary2005@gmail.com>
2024-09-14 16:49:41 +0530

    :star: new route added

--- app/home/page.js
+++ app/home/page.js
@@ -1,9 +1,12 @@
+import Layout from '@/components/Layout'
 import React from 'react'
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
+    return (
+        <Layout>
+            <div>Home</div>
+        </Layout>
+    )
 }
 
 export default Home
